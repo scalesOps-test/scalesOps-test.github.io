e3cb4dca9663b56d48f1decd8a3a790be74bacdf
scalesOps-test <144047459+scalesOps-test@users.noreply.github.com>
2023-09-07 18:13:39 +0330

    html commit

--- controllers/error.js
+++
@@ -1,6 +0,0 @@
-exports.get404 = (req, res) => {
-    res.status(404);
-    res.render('404', {
-        pageTitle: "صفحه مورد نظر پیدا نشد"
-    })
-}
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const todosController = require("../controllers/todos");
-
-const router = express.Router();
-
-router.get("/", todosController.getIndex);
-
-module.exports = router;
